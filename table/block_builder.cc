2efa40df47d2d10a270d0a935f7fe4d29499bc3b
ZheNing Hu <adlternative@gmail.com>
2021-08-10 09:58:42 +0800

    block builder
    |0|key.length|value.length|key|value|(restart_point 1)
    |key.shared.length|key.non_shared.length|value.length|key.non_shared|value|
    |key.shared.length|key.non_shared.length|value.length|key.non_shared|value|
    |key.shared.length|key.non_shared.length|value.length|key.non_shared|value|
    ...
    |0|key.length|value.length|key|value|(restart_point 2)
    |key.shared.length|key.non_shared.length|value.length|key.non_shared|value|
    |key.shared.length|key.non_shared.length|value.length|key.non_shared|value|
    |key.shared.length|key.non_shared.length|value.length|key.non_shared|value|
    |restarts_point_1.offset|
    |restarts_point_2.offset|
    ...
    |restarts_points.length|
    
    Signed-off-by: ZheNing Hu <adlternative@gmail.com>

--- table/block_builder.cc
+++ table/block_builder.cc
@@ -33,6 +33,7 @@
 
 #include "leveldb/comparator.h"
 #include "leveldb/options.h"
+
 #include "util/coding.h"
 
 namespace leveldb {
@@ -52,6 +53,7 @@ void BlockBuilder::Reset() {
   last_key_.clear();
 }
 
+/* 此时调用 finish 的 buffer 总大小 */
 size_t BlockBuilder::CurrentSizeEstimate() const {
   return (buffer_.size() +                       // Raw data buffer
           restarts_.size() * sizeof(uint32_t) +  // Restart array
@@ -60,11 +62,14 @@ size_t BlockBuilder::CurrentSizeEstimate() const {
 
 Slice BlockBuilder::Finish() {
   // Append restart array
+  /* 添加重启点数组 */
   for (size_t i = 0; i < restarts_.size(); i++) {
     PutFixed32(&buffer_, restarts_[i]);
   }
+  /* 添加重启数组大小 */
   PutFixed32(&buffer_, restarts_.size());
   finished_ = true;
+  /* 返回构建的 kvs block */
   return Slice(buffer_);
 }
 
@@ -72,33 +77,47 @@ void BlockBuilder::Add(const Slice& key, const Slice& value) {
   Slice last_key_piece(last_key_);
   assert(!finished_);
   assert(counter_ <= options_->block_restart_interval);
+  /* 保证 key 升序 */
   assert(buffer_.empty()  // No values yet?
          || options_->comparator->Compare(key, last_key_piece) > 0);
   size_t shared = 0;
+  /* 目前的项数 % 16 < 16 */
   if (counter_ < options_->block_restart_interval) {
     // See how much sharing to do with previous string
+    /* 找到当前 key 字符串长度和 last_key_piece 长度的最小值 */
     const size_t min_length = std::min(last_key_piece.size(), key.size());
+    /* 寻找共有的前缀长度 */
     while ((shared < min_length) && (last_key_piece[shared] == key[shared])) {
       shared++;
     }
   } else {
+    /* 目前的项数 % 16 == 0  */
     // Restart compression
+    /* 重启点列表添加新的重启点偏移量 */
+    /* 注意到这样 shared = 0 则全量存储 */
     restarts_.push_back(buffer_.size());
     counter_ = 0;
   }
+  /* 当前key中 不共享长度 */
   const size_t non_shared = key.size() - shared;
 
   // Add "<shared><non_shared><value_size>" to buffer_
+  /* 记录共享长度和不共享长度和值长度 */
   PutVarint32(&buffer_, shared);
   PutVarint32(&buffer_, non_shared);
   PutVarint32(&buffer_, value.size());
 
   // Add string delta to buffer_ followed by value
+  /* 记录 key 不共享部分 */
   buffer_.append(key.data() + shared, non_shared);
+  /* 记录 value */
   buffer_.append(value.data(), value.size());
 
   // Update state
+  /* 将 last_key 先切成和当前 key 的共享部分 */
   last_key_.resize(shared);
+  /* 再添加当前 key 的私有部分 */
+  /* 其实就是让 last_key = key */
   last_key_.append(key.data() + shared, non_shared);
   assert(Slice(last_key_) == key);
   counter_++;
--- table/block_builder.h
+++ table/block_builder.h
@@ -22,22 +22,30 @@ class BlockBuilder {
   BlockBuilder& operator=(const BlockBuilder&) = delete;
 
   // Reset the contents as if the BlockBuilder was just constructed.
+  // 重置内容，就像刚刚构造了 BlockBuilder 一样。
   void Reset();
 
   // REQUIRES: Finish() has not been called since the last call to Reset().
   // REQUIRES: key is larger than any previously added key
+  // 要求：自上次调用 Reset() 以来，尚未调用 Finish()。
+  // 要求：key 大于任何先前添加的 key
   void Add(const Slice& key, const Slice& value);
 
   // Finish building the block and return a slice that refers to the
   // block contents.  The returned slice will remain valid for the
   // lifetime of this builder or until Reset() is called.
+  // 完成块的构建并返回一个引用块的切片块内容。
+  // 返回的切片将在blockbuilder的生命周期间保持有效
+  // 除非调用 Reset()。
   Slice Finish();
 
   // Returns an estimate of the current (uncompressed) size of the block
   // we are building.
+  // 返回我们正在构建的块的当前（未压缩）大小的估计值。
   size_t CurrentSizeEstimate() const;
 
   // Return true iff no entries have been added since the last Reset()
+  // 如果自上次 Reset() 以来没有添加任何条目，则返回 true
   bool empty() const { return buffer_.empty(); }
 
  private:
@@ -46,7 +54,7 @@ class BlockBuilder {
   std::vector<uint32_t> restarts_;  // Restart points
   int counter_;                     // Number of entries emitted since restart
   bool finished_;                   // Has Finish() been called?
-  std::string last_key_;
+  std::string last_key_;            // 记录最后添加的key
 };
 
 }  // namespace leveldb
