3f1acc1180e728c946b5e75dde17406f49e51d90
ZheNing Hu <adlternative@gmail.com>
2021-12-02 21:41:46 +0800

    test
    Signed-off-by: ZheNing Hu <adlternative@gmail.com>

---
+++ test.cc
@@ -0,0 +1,153 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include <string>
+
+#include "leveldb/options.h"
+
+#include "./include/leveldb/db.h"
+using namespace std;
+
+TEST(leveldb, OpenTest) {
+  leveldb::DB* db;
+  leveldb::Options options;
+  options.create_if_missing = true;
+  auto path = "/tmp/testdb4";
+  ASSERT_TRUE(leveldb::DB::Open(options, path, &db).ok())
+      << path << " open failed!";
+  delete db;
+}
+
+TEST(leveldb, PutTest) {
+  leveldb::DB* db;
+  leveldb::Options options;
+
+  options.create_if_missing = true;
+  auto status = leveldb::DB::Open(options, "/tmp/testdb3", &db);
+  if (!status.ok()) {
+    std::cout << status.ToString() << std::endl;
+    return;
+  }
+
+  string result_val;
+  string random_key = "key" + to_string(rand());
+  status = db->Put(leveldb::WriteOptions(), random_key, "hihi");
+  if (!status.ok()) {
+    printf("put failed! err:%s\n", status.ToString().c_str());
+  }
+
+  delete db;
+}
+
+TEST(leveldb, GetTest) {
+  leveldb::DB* db;
+  leveldb::Options options;
+
+  options.create_if_missing = true;
+  auto status = leveldb::DB::Open(options, "/tmp/testdb3", &db);
+  if (!status.ok()) {
+    std::cout << status.ToString() << std::endl;
+    return;
+  }
+
+  string result_val;
+  status = db->Put(leveldb::WriteOptions(), "hehe", "hihi");
+  if (!status.ok()) {
+    printf("put failed! err:%s\n", status.ToString().c_str());
+  } else {
+    printf("put ok!\n");
+  }
+
+  db->Get(leveldb::ReadOptions(), "hehe", &result_val);
+  printf("[%s %s]\n", "heheh", result_val.c_str());
+  if (!status.ok()) {
+    printf("get failed! err:%s\n", status.ToString().c_str());
+  } else {
+    printf("get ok!\n");
+  }
+  delete db;
+}
+
+TEST(leveldb, PutTest2) {
+  leveldb::DB* db;
+  leveldb::Options options;
+
+  options.create_if_missing = true;
+  auto status = leveldb::DB::Open(options, "/tmp/testdb3", &db);
+  if (!status.ok()) {
+    std::cout << status.ToString() << std::endl;
+    return;
+  }
+
+  string result_val;
+  for (int i = 0; i < 1000000; ++i) {
+    string key = to_string(i);
+    EXPECT_TRUE(db->Put(leveldb::WriteOptions(), key, "hihi").ok());
+  }
+
+  delete db;
+}
+
+TEST(leveldb, PutTest3) {
+  leveldb::DB* db;
+  leveldb::Options options;
+
+  options.create_if_missing = true;
+  auto status = leveldb::DB::Open(options, "/tmp/testdb13", &db);
+  if (!status.ok()) {
+    std::cout << status.ToString() << std::endl;
+    return;
+  }
+
+  string result_val;
+  for (int i = 1000000; i >= 0; --i) {
+    string key = to_string(i);
+    EXPECT_TRUE(db->Put(leveldb::WriteOptions(), key, "hihi").ok());
+  }
+
+  delete db;
+}
+
+TEST(leveldb, BigKeyPut) {
+  leveldb::DB* db;
+  leveldb::Options options;
+
+  options.create_if_missing = true;
+  auto status = leveldb::DB::Open(options, "/tmp/testdb14", &db);
+  if (!status.ok()) {
+    std::cout << status.ToString() << std::endl;
+    return;
+  }
+
+  string key(1000000, 'a');
+  for (int i = 0; i < 7; i++) {
+    key.append(key);
+  }
+  std::cout << key.size() << std::endl;
+  EXPECT_TRUE(db->Put(leveldb::WriteOptions(), key, "hihi").ok());
+  // EXPECT_TRUE(db->Get(leveldb::ReadOptions(), key, val).ok());
+  delete db;
+}
+
+TEST(leveldb, BigKeyGet) {
+  leveldb::DB* db;
+  leveldb::Options options;
+
+  options.create_if_missing = true;
+  auto status = leveldb::DB::Open(options, "/tmp/testdb14", &db);
+  if (!status.ok()) {
+    std::cout << status.ToString() << std::endl;
+    return;
+  }
+
+  string key(1000000, 'a');
+  for (int i = 0; i < 7; i++) {
+    key.append(key);
+  }
+  std::cout << key.size() << std::endl;
+
+  string val;
+  auto rc = db->Get(leveldb::ReadOptions(), key, &val);
+  EXPECT_TRUE(rc.ok()) << rc.ToString() << std::endl;
+  EXPECT_EQ(val, "hihi");
+  delete db;
+}
